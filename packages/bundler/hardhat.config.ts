dadb98d6e596a9b493c0db5123fca90221d82fa7
kumar harsh <harsh@kumars-MacBook-Air.local>
2023-02-20 15:23:55 +0530

    first commit

---
+++ packages/bundler/hardhat.config.ts
@@ -0,0 +1,52 @@
+import '@nomiclabs/hardhat-ethers'
+import '@nomicfoundation/hardhat-toolbox'
+import 'hardhat-deploy'
+
+import fs from 'fs'
+
+import { HardhatUserConfig } from 'hardhat/config'
+import { NetworkUserConfig } from 'hardhat/src/types/config'
+
+const mnemonicFileName = './localconfig/mnemonic.txt'
+let mnemonic = 'test '.repeat(11) + 'junk'
+if (mnemonicFileName != null && fs.existsSync(mnemonicFileName)) {
+  mnemonic = fs.readFileSync(mnemonicFileName, 'ascii').trim()
+}
+
+const infuraUrl = (name: string): string => `https://${name}.infura.io/v3/be456f1154af4971877c7c52dd5a612a`
+
+function getNetwork (url: string): NetworkUserConfig {
+  console.log(mnemonic)
+  return {
+    url,
+    accounts: 
+      [mnemonic]
+  }
+}
+
+function getInfuraNetwork (name: string): NetworkUserConfig {
+  console.log(`infura: ${infuraUrl(name)}`)
+  return getNetwork(infuraUrl(name))
+}
+
+const config: HardhatUserConfig = {
+  typechain: {
+    outDir: 'src/types',
+    target: 'ethers-v5'
+  },
+  networks: {
+    localhost: {
+      url: 'http://localhost:8545/',
+      saveDeployments: false
+    },
+    goerli: getInfuraNetwork('goerli')
+  },
+  solidity: {
+    version: '0.8.15',
+    settings: {
+      optimizer: { enabled: true }
+    }
+  }
+}
+
+export default config
